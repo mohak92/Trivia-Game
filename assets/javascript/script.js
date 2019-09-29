00caae734ff77fbbfd3310a4c497dcb7a76f864e
mohak92 <tamhane.mohak@gmail.com>
2019-09-28 22:59:55 -0400

    display questions, category and possible answers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 var correctAnswer;
-
+var questionsToDisp = $('<div>');
+var answersToDisp = $('<div>');
 document.addEventListener("DOMContentLoaded", function(){
     loadQuestion();
 });
@@ -13,20 +14,51 @@ var loadQuestion = function() {
         method: "GET"
     }).then(function (response) {
         displayQuestions(response.results);
+        displayAnswers(response.results);
     });
 }
 
 function displayQuestions(questions){
-    //var questionsToDisp = $('<div>');
-   // quastionsToDisp.addClass('col-12');
+    questionsToDisp.addClass('col-12');
 
-    for(var i = 0; i < questions.length;i++){
+    for(var i = 0; i < questions.length; i++){
         correctAnswer = questions[i].correct_answer;
 
-        let possibleAnswers = questions[i].incorrect_answers;
+        var myQuestions = $('<div>');
+        myQuestions.addClass("row justify-content-between heading");
+        var dispCategory = $('<p>');
+        dispCategory.addClass("category");
+        dispCategory.text("Category: " + questions[i].category);
+        myQuestions.append(dispCategory);
+        questionsToDisp.append(myQuestions);
+        var dispQuestion = $('<h2>');
+        dispQuestion.addClass("text-center");
+        dispQuestion.html(questions[i].question);
+        questionsToDisp.append(dispQuestion);
+        
+        $("#app").prepend(questionsToDisp);
+    }
+
+}
+
+function displayAnswers(questions){
+    answersToDisp.addClass('col-12');
+    for(var i = 0; i < questions.length; i++){
+        var possibleAnswers = questions[i].incorrect_answers;
         possibleAnswers.splice( Math.floor( Math.random() * 3 ), 0, correctAnswer);
 
-        console.log(possibleAnswers);
-        console.log(questions[i]);
+        var answersDiv= $('<div>');
+        answersDiv.addClass('questions row justify-content-around mt-4');
+        for(var j = 0; j < possibleAnswers.length; j++){
+            var answerHTML = $('<li>');
+            answerHTML.addClass('col-12 col-md-5');
+            answerHTML.text(possibleAnswers[j]);
+            console.log(possibleAnswers[j]);
+            //$("li").on("click", selectAnswer);
+            answersDiv.prepend(answerHTML);
+        }
+
+        answersToDisp.prepend(answersDiv);
+        $("#app2").prepend(answersToDisp);
     }
 }
