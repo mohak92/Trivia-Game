504536d111f4e8440445256e9083c3b09e511000
mohak92 <tamhane.mohak@gmail.com>
2019-09-28 20:54:18 -0400

    ajax call using open trivia db to get questions for trivia game apps

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("ready");
+    loadQuestion();
 });
+
+var loadQuestion = function() {
+    console.log("from load question");
+    var queryURL = "https://opentdb.com/api.php?amount=1";
+    // Creates AJAX call for questions
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        displayQuestions(response.results);
+    });
+}
+
+function displayQuestions(questions){
+    console.log(questions);
+}
